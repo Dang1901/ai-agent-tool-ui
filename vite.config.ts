ffe15a2be2f267a0f537fc63850044188403768a
Dangph21 <dangph21@fpt.com>
2025-09-07 19:03:46 +0700

    fix config vite path v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@app': path.resolve(__dirname, 'src/app'),
-      '@api': path.resolve(__dirname, 'src/api'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@router': path.resolve(__dirname, 'src/router'),
-      '@store': path.resolve(__dirname, 'src/store'),
-      '@types': path.resolve(__dirname, 'src/types'),
-      '@applications': path.resolve(__dirname, 'src/applications'),
-    },
+      alias: {
+        '@app': '/src/app',
+        '@api': '/src/api',
+        '@assets': '/src/assets',
+        '@components': '/src/components',
+        '@features': '/src/features',
+        '@hooks': '/src/hooks',
+        '@pages': '/src/pages',
+        '@router': '/src/router',
+        '@store': '/src/store',
+        '@types': '/src/types',
+        '@applications': '/src/applications',
+      },
   },
 })
